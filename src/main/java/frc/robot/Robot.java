c0520f62b33301af988214bc7e89cbd5854188f7
anonimous-user <harrisbond1234@gmail.com>
2023-03-04 23:20:55 -0500

    attempt at something

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -51,17 +51,20 @@ public class Robot extends TimedRobot {
     private static final double kMaxOutput = 0.5; // Maximum output
     private static final double kMinOutput = -0.5; // Minimum output
   }
+  
   /*
+  double ConversionFactor = 10;//check multiplier to have it be in meters or inches or smth
+
   DoubleSolenoid reach = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
   Value state = Value.kReverse;
 
-  CANSparkMax arm = new CANSparkMax(0, MotorType.kBrushless);
+  CANSparkMax arm = new CANSparkMax(5, MotorType.kBrushless);
   RelativeEncoder armEncoder = arm.getEncoder();
   
   CANSparkMax intake = new CANSparkMax(0, MotorType.kBrushless);
   RelativeEncoder intakeEncoder = intake.getEncoder();
   
-  CANSparkMax elevator = new CANSparkMax(5, MotorType.kBrushless);
+  CANSparkMax elevator = new CANSparkMax(0, MotorType.kBrushless);
   RelativeEncoder elevatorEncoder = elevator.getEncoder();
   */
   static ADIS16470_IMU gyro = new ADIS16470_IMU();
@@ -79,54 +82,79 @@ public class Robot extends TimedRobot {
     timer.restart();
 
     rightBack.restoreFactoryDefaults();
-    rightFront.restoreFactoryDefaults();
-    leftBack.restoreFactoryDefaults();
-    leftFront.restoreFactoryDefaults();
-
     rightBack.setSmartCurrentLimit(35);
-    rightFront.setSmartCurrentLimit(35);
-    leftBack.setSmartCurrentLimit(35);
-    leftFront.setSmartCurrentLimit(35);
-
     rightBack.setIdleMode(IdleMode.kBrake);
-    rightFront.setIdleMode(IdleMode.kBrake);
-    leftBack.setIdleMode(IdleMode.kBrake);
-    leftFront.setIdleMode(IdleMode.kBrake);
-
     rightBack.set(0);
+
+    rightFront.restoreFactoryDefaults();
+    rightFront.setSmartCurrentLimit(35);
+    rightFront.setIdleMode(IdleMode.kBrake);
     rightFront.set(0);
+
+    leftBack.restoreFactoryDefaults();
+    leftBack.setSmartCurrentLimit(35);
+    leftBack.setIdleMode(IdleMode.kBrake);
     leftBack.set(0);
+
+    leftFront.restoreFactoryDefaults();
+    leftFront.setSmartCurrentLimit(35);
+    leftFront.setIdleMode(IdleMode.kBrake);
     leftFront.set(0);
 
     rightBack.setInverted(true);
     rightFront.setInverted(true);
 
 
-    /*
     gyro.reset();
     gyro.calibrate();
-
+    
+    
+    /*
     elevator.restoreFactoryDefaults();
+    elevator.setSmartCurrentLimit(35);
+    elevator.setIdleMode(IdleMode.kBrake);
+    elevator.set(0);
+    elevator.enableSoftLimit(CANSparkMax.SoftLimitDirection.kForward, true);
+    elevator.enableSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, true);
+    elevator.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, 10);//check limit value
+    elevator.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, 10);//check limit value
     elevatorEncoder.setPosition(0);
-    elevatorEncoder.setPositionConversionFactor(10);
-    elevatorEncoder.setVelocityConversionFactor(1000 / 60);
+    elevatorEncoder.setPositionConversionFactor(ConversionFactor);
+    //elevatorEncoder.setVelocityConversionFactor(1000 / 60);
+
 
     arm.restoreFactoryDefaults();
+    arm.setSmartCurrentLimit(20);
+    arm.setIdleMode(IdleMode.kBrake);
+    arm.set(0);
+    arm.enableSoftLimit(CANSparkMax.SoftLimitDirection.kForward, true);
+    arm.enableSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, true);
+    arm.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, 10);//check limit value
+    arm.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, 10);//check limit value
     armEncoder.setPosition(0);
-    armEncoder.setPositionConversionFactor(10);
-    armEncoder.setVelocityConversionFactor(1000 / 60);
+    armEncoder.setPositionConversionFactor(ConversionFactor);
+    //armEncoder.setVelocityConversionFactor(1000 / 60);
+
 
     intake.restoreFactoryDefaults();
+    intake.setSmartCurrentLimit(15);
+    intake.setIdleMode(IdleMode.kBrake);
+    intake.set(0);
+    intake.enableSoftLimit(CANSparkMax.SoftLimitDirection.kForward, true);
+    intake.enableSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, true);
+    intake.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, 10);//check limit value
+    intake.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, 10);//check limit value
     intakeEncoder.setPosition(0);
-    intakeEncoder.setPositionConversionFactor(10);
-    intakeEncoder.setVelocityConversionFactor(1000 / 60);
+    intakeEncoder.setPositionConversionFactor(ConversionFactor);
+    //intakeEncoder.setVelocityConversionFactor(1000 / 60);
+
 
     reach.set(Value.kReverse);
     state = Value.kReverse;
 
+
     UsbCamera cam = CameraServer.startAutomaticCapture(0);
     cam.setResolution(283,160);
-
     */
   }
 
@@ -159,18 +187,29 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    //drives forward for 2 seconds
-    if(timer.get()<0.5){
-      driveMecanum(0, -0.75, 0);
+/*
+    //Platform balance attempt, must be straight and right in front
+    if(timer.get()<0.3){
+      driveMecanum(0, 1, 0);
+    } else if(timer.get()<1.5){
+      driveMecanum(0, 0, 0);
     } else if(timer.get()<2){
-      leftBack.set(1);
-      rightBack.set(1);
-      leftFront.set(1);
-      rightFront.set(1);
+      driveMecanum(0, 1, 0);
+    } else if(timer.get()<2.2){
+      driveMecanum(0, -0.1, 0);
     } else{
       driveMecanum(0, 0, 0);
     }
+ */
 
+    //simple auto
+    if(timer.get()<0.5){
+      driveMecanum(0, -0.5, 0);
+    } else if(timer.get()<2){
+      driveMecanum(0, 1, 0);
+    } else{
+      driveMecanum(0, 0, 0);
+    }
   }
 
   @Override
@@ -184,14 +223,19 @@ public class Robot extends TimedRobot {
     //mdrive.driveCartesian(-threshold(c1.getLeftY()), -threshold(c1.getLeftX()), threshold(c1.getRightX())*0.67);
 
     SmartDashboard.putNumber("angle y", getPitch());
-    SmartDashboard.putNumber("angle x", gyro.getXComplementaryAngle());
+    SmartDashboard.putNumber("angle x", gyro.getAngle());
     
     
     //balance
     if(c1.getBButton()){
       balanceOnStation();
     }
+
 /* 
+    if(c1.getXButton()){
+      turn180();
+    }
+
     //pneumatic
     if(c2.getBButton()){
         if(state==Value.kReverse){
@@ -219,7 +263,7 @@ public class Robot extends TimedRobot {
     } else{
       intake.set(0);
     }
-
+    
     //elevator
     if(c2.getYButton()){
       elevator.set(0.75);
@@ -232,12 +276,21 @@ public class Robot extends TimedRobot {
   
   }
 
-
+  /*
+    public void turn180(){
+      timer.reset();
+      gyro.reset();
+      while(timer.get()<1&&gyro.getAngle()<180){
+        driveMecanum(0, 0, 0.5);
+      }
+    }
+  */
+  
   public void driveMecanum(double xSpeed, double ySpeed, double zRot) {
     // Calculate the angle and magnitude of the joystick input
     double theta = Math.atan2(ySpeed, xSpeed);
     double power = Math.hypot(xSpeed, ySpeed);
-
+    
     // Calculate the sine and cosine of the angle, offset by 45 degrees
     double sin = Math.sin(theta - Math.PI / 4);
     double cos = Math.cos(theta - Math.PI / 4);
@@ -255,7 +308,7 @@ public class Robot extends TimedRobot {
         rightFront /= power + Math.abs(zRot);
         leftRear /= power + Math.abs(zRot);
         rightRear /= power + Math.abs(zRot);
-    }
+      }
 
     if(leftFront==0&&leftRear==0&&rightFront==0&&rightRear==0){
       this.leftFront.stopMotor();
@@ -310,7 +363,7 @@ public class Robot extends TimedRobot {
     double angle = getPitch();
     double error = -angle;
     if (Math.abs(error) < DrivetrainConstants.kToleranceDegrees) {
-        setMotorSpeeds(0, 0, 0, 0);
+      setMotorSpeeds(0, 0, 0, 0);
     }
     double output = DrivetrainConstants.kProportionalGain*error+DrivetrainConstants.kIntegralGain*DrivetrainConstants.integralError+DrivetrainConstants.kDerivativeGain*(error-DrivetrainConstants.previousError);
     output = Math.max(DrivetrainConstants.kMinOutput, Math.min(DrivetrainConstants.kMaxOutput, output));
@@ -322,24 +375,24 @@ public class Robot extends TimedRobot {
     return Math.atan2(gyro.getAccelY(), gyro.getAccelZ()) * 180 / Math.PI;
   }
   
+  
+
+  
+
+  
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+  
+  
+  
+  
+  
+  
+  
   @Override
   public void disabledInit() {}
-
+  
   @Override
   public void disabledPeriodic() {}
 
